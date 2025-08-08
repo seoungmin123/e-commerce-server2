01532d911524a8312b80d5a503a66ac8dc2e6ea2
seoungmin123 <sm1111176@gmail.com>
2025-08-08 15:59:24 +0900

    style : 코드 변경

--- src/main/java/kr/hhplus/be/server/product/dto/ProductInfo.java
+++ src/main/java/kr/hhplus/be/server/product/dto/ProductInfo.java
@@ -2,16 +2,24 @@ package kr.hhplus.be.server.product.dto;
 
 import kr.hhplus.be.server.product.domain.Product;
 import kr.hhplus.be.server.product.domain.ProductStock;
+import lombok.Builder;
 
 import java.math.BigDecimal;
 
+@Builder
 public record ProductInfo(
         Long productId,
         String name,
         BigDecimal price,
         int stock
 ) {
-    public static ProductInfo of(Product product, ProductStock stock) {
-        return new ProductInfo(product.getId(), product.getName(), product.getPrice(), stock.getQuantity());
+    public static ProductInfo of(Product product, ProductStock stock){
+        return ProductInfo.builder()
+                .productId(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .stock(stock.getQuantity())
+                .build();
     }
+
 }
